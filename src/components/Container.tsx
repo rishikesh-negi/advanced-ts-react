42f4d0b54ba5658f3c4627d7e730310ce75a1719
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-16 19:51:40 +0530

    Created a polymorphic component to learn about the said component type in React + TS

---
+++ src/components/Container.tsx
@@ -0,0 +1,19 @@
+import type { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+
+// Creating a polymorphic component - a wrapper component whose wrapped component/s are not known in advance:
+
+// React's built-in ElementType type restricts the type to a valid JSX element identifier (div, main, aside, etc.) or an existing React functional component (like Button):
+type ContainerProps<T extends ElementType> = {
+  asElement?: T;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+// The ComponentPropsWithoutRef<T> type doesn't just accept JSX element identifiers as argument for its type parameter, but also custom React functional components.
+
+export default function Container<C extends ElementType>({
+  asElement,
+  children,
+  ...props
+}: ContainerProps<C>) {
+  const Component = asElement || "div";
+  return <Component {...props}>{children}</Component>;
+}
