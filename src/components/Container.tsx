ae1a27f4aa30d0f1fb5626f851f4d2bea7bce84f
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-19 19:20:10 +0530

    Fixed/inproved explanatory comment

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import type { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
 
 // Creating a polymorphic component - a wrapper component whose wrapped component/s are not known in advance:
 
-// React's built-in ElementType type restricts the type to a valid JSX element (div, main, aside, etc.) or an existing React functional component (like Button). The "T" type parameter is used as the identifier and placeholder for the type of the value the "asElement" prop will receive:
+// React's built-in ElementType type restricts the type to a valid JSX element (div, main, aside, etc.) or an existing React functional component (like Button). The "T" type parameter is used as the identifier and placeholder for the type of the incoming argument of the "asElement" prop:
 type ContainerProps<T extends ElementType> = {
   asElement?: T;
   children: ReactNode;
