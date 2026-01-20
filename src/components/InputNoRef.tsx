a75850274b9df64d0795825224e043757d5acac3
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-20 18:11:23 +0530

    Renamed file to distinguish it from its ref-forwarding version

--- src/components/InputNoRef.tsx
+++ src/components/InputNoRef.tsx
@@ -1,13 +1,13 @@
 import type { ComponentPropsWithoutRef, FC } from "react";
 
 // If we want our props object to include any attribute/prop usable in the "label" or "input" element, it's not practical to add and type every single one of them in our props object. Instead, we type the custom props the component should receive and intersect it with React's built-in ComponentProps<T> type or one of its variations:
-type InputProps = {
+type InputNoRefProps = {
   label: string;
   id: string;
 } & ComponentPropsWithoutRef<"input">;
 // The ComponentPropsWithRef<T> type adds a "ref" prop to the props object - the ComponentPropsWithoutRef<T> does not. The added "ref" prop only works if we're forwarding refs to the component. These utility types accept a string containing the name of an HTML element as an input for their type parameter.
 
-const Input: FC<InputProps> = function ({ label, id }) {
+const InputNoRef: FC<InputNoRefProps> = function ({ label, id }) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
@@ -16,4 +16,4 @@ const Input: FC<InputProps> = function ({ label, id }) {
   );
 };
 
-export default Input;
+export default InputNoRef;
