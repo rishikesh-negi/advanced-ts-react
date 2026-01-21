95c3af02eb63d75dbdc3db73c89204505b0f3aef
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-21 19:49:05 +0530

    Added a form component that exposes a callable API via the useImperativeHandle hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import InputWithRef from "./components/InputWithRef";
-import Form from "./components/Form";
+import Form, { type FormApi } from "./components/FormWithExposedAPI";
 
 function App() {
   // We should provide the type argument to useRef to indicate that the ref will eventually be the "input" HTML element:
   const inputRef = useRef<HTMLInputElement>(null);
 
+  // The ref to connect a component instance with the API it exposes to use the exposed API:
+  const formWithApiRef = useRef<FormApi>(null);
+
   // In this function, we have to deal with the fact that the type of "data" is not known. But, "data" is unknown only in the Form component. We do know the shape of the data here, because this is where the input fields of Form are known. So, here we need to convince TS that we know the type of "data". So, we can use the "as" assertion:
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
-    // Important to remember that even though we set the "type" attribute of an input field to number, the type of the data will still be string, which is why the "age" property is set to the type of "string" in the above assertion.
+    // Important to remember: Even if we set the "type" attribute of an input field to number, the data type of its value will still be string, which is why the "age" property is set to the type of "string" in the above assertion.
 
     console.log(extractedData);
+
+    formWithApiRef?.current?.clear();
   }
 
   return (
@@ -29,7 +34,7 @@ function App() {
 
       <InputWithRef label="Test" id="test-ref-forwarding" ref={inputRef} />
 
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={formWithApiRef}>
         <InputWithRef type="text" label="Name" id="name" />
         <InputWithRef type="number" label="Age" id="age" />
         <p>
