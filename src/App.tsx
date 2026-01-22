f47253130445287f181a6660cc6b1931126c8430
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-22 16:57:12 +0530

    Added type narrowing with type guards as an alternative to type assertion for determining the type of the form data object

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,24 @@ function App() {
 
   // In this function, we have to deal with the fact that the type of "data" is not known. But, "data" is unknown only in the Form component. We do know the shape of the data here, because this is where the input fields of Form are known. So, here we need to convince TS that we know the type of "data". So, we can use the "as" assertion:
   function handleSave(data: unknown) {
+    // Type assertion for when TS can't infer the type but we're 100% sure about the type:
     const extractedData = data as { name: string; age: string };
     // Important to remember: Even if we set the "type" attribute of an input field to number, the data type of its value will still be string, which is why the "age" property is set to the type of "string" in the above assertion.
-
     console.log(extractedData);
 
+    // Type guards for type narrowing instead of using type assertions:
+    if (
+      !data ||
+      typeof data !== "object" ||
+      !("name" in data) ||
+      !("age" in data)
+    )
+      return;
+
+    // Beyond this point, because of the above type narrowing, TS knows that data: exists, is an object, and has "name" and "age" properties:
+    console.log(data);
+
+    // Calling a method of the Form component's exposed API using a ref:
     formWithApiRef?.current?.clear();
   }
 
